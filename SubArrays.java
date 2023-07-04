9fa4587e6e0efa8ead92f1be319f377fbf8d9508
chaamu <125999531+chaamu@users.noreply.github.com>
2023-07-04 23:08:10 +0530

    Add files via upload

---
+++ SubArrays.java
@@ -0,0 +1,24 @@
+public class SubArrays{
+    public static void PrintSubarrays(int numbers[]){
+        int st=0;
+
+        for (int i=0; i<numbers.length; i++){
+            int start= i;
+            for (int j=i; j<numbers.length; j++){
+            int end= j;
+            for (int k=start; k<=end; k++){
+                System.out.print(numbers[k]+ " ");
+                st++;
+            }
+            System.out.println();
+        } 
+        System.out.println();
+       }
+       System.out.println("total subarray=" +st);
+}
+    public static void main(String[] args){
+        int numbers[]={2,4,6,8,9};
+        PrintSubarrays(numbers);
+        
+    }
+}
